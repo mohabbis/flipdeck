f2d8d4466dfe94387cabf810360418259c4e173e
Muhammad Rafiq <mohabbis@users.noreply.github.com>
2026-05-31 23:58:07 +0000

    fix: Align settings.c with settings.h structure
    Remove fields not present in settings.h (show_icons, show_descriptions,
    startup_category, long_snippet_warn_state are in flipdeck_app.h settings)

--- src/settings.c
+++ src/settings.c
@@ -22,9 +22,5 @@ bool settings_save(FlipDeckSettings* settings) {
 void settings_reset_to_defaults(FlipDeckSettings* settings) {
     settings->send_delay_ms = 100;
     settings->confirm_before_send = true;
-    settings->auto_detect_usb = true;
-    settings->show_icons = true;
-    settings->show_descriptions = true;
-    settings->long_snippet_warn_state = 0;
-    strcpy(settings->startup_category, "");
+    settings->auto_connect_usb = true;
 }
